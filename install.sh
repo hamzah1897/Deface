b0f09c57d242aaf4c33d2c197708a7a9482f935c
hamzah1897 <74802638+hamzah1897@users.noreply.github.com>
2020-11-27 10:09:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-pkg update && pkg upgrade && pkg install curl && pkg install figlet && pkg install git && pkg install python2 && gem install lolcat
+pkg update && pkg upgrade && pkg install curl && pkg install figlet && pkg install git && pkg install python2 && gem install lolcat && sh deface.sh
