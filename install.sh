488b83b07aced0fe0e11da690e0aaedf1f2d2f43
hamzah1897 <74802638+hamzah1897@users.noreply.github.com>
2020-11-27 10:52:21 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-apt -o Acquire::EnableSrvRecords=false upgrade && pkg upgrade && pkg install curl && pkg install figlet && pkg install git && pkg install python2 && gem install lolcat && sh deface.sh
+pkg update && pkg upgrade && pkg install curl && pkg install figlet && pkg install git && pkg install python2 && gem install lolcat && sh deface.sh
